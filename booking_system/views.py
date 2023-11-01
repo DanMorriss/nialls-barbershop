9af2611fcd9ccef2afdee9234f5cf685be41ad70
Dan Morriss <danielmorriss1@gmail.com>
2023-11-01 22:17:11 +0000

    Create index.html and urls.py

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class HomeView(generic.ListView):
+    model = 
+    template_name = 'index.html'
