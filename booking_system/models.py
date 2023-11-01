dfa6fdc4e7d9ce5477aece3a0776272daabf8930
Dan Morriss <danielmorriss1@gmail.com>
2023-11-01 16:36:41 +0000

    Add models for service and booking

--- booking_system/models.py
+++ booking_system/models.py
@@ -1,15 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
-BOOKING_TIME = ((1, "9:00am"), (2, "9:15am"), (3, "9:30am"), (4, "9:45am"),
-                (5, "10:00am"), (6, "10:15am"), (7, "10:30am"), (8, "10:45am"),
-                (9, "11:00am"), (10, "11:15am"), (11, "11:30am"),
-                (12, "11:45am"), (13, "12:00pm"), (14, "12:15pm"),
-                (15, "12:30pm"), (16, "12:45pm"), (17, "2:00pm"),
-                (18, "2:15pm"), (19, "2:30pm"), (20, "2:45pm"),
-                (21, "3:00pm"), (22, "3:15pm"), (23, "3:30pm"), (24, "3:45pm"),
-                (25, "4:00pm"), (26, "4:15pm"), (27, "4:30pm"), (28, "4:45pm"),
-                (25, "4:00pm"), (26, "4:15pm"), (27, "4:30pm"))
+BOOKING_TIME = ((datetime.time(9, 0, 0), '9:00am'),
+                (datetime.time(9, 15, 0), '9:15am'),
+                (datetime.time(9, 30, 0), '9:30am'),
+                (datetime.time(9, 45, 0), '9:45am'),
+                (datetime.time(10, 0, 0), '10:00am'),
+                (datetime.time(10, 15, 0), '10:15am'),
+                (datetime.time(10, 30, 0), '10:30am'),
+                (datetime.time(10, 45, 0), '10:45am'),
+                (datetime.time(11, 0, 0), '11:00am'),
+                (datetime.time(11, 15, 0), '11:15am'),
+                (datetime.time(11, 30, 0), '11:30am'),
+                (datetime.time(11, 45, 0), '11:45am'),
+                (datetime.time(12, 0, 0), '12:00pm'),
+                (datetime.time(12, 15, 0), '12:15pm'),
+                (datetime.time(12, 30, 0), '12:30pm'),
+                (datetime.time(12, 45, 0), '12:45pm'),
+                (datetime.time(13, 0, 0), '1:00pm'),
+                (datetime.time(13, 15, 0), '1:15pm'),
+                (datetime.time(13, 30, 0), '1:30pm'),
+                (datetime.time(13, 45, 0), '1:45pm'),
+                (datetime.time(14, 0, 0), '2:00pm'),
+                (datetime.time(14, 15, 0), '2:15pm'),
+                (datetime.time(14, 30, 0), '2:30pm'),
+                (datetime.time(14, 45, 0), '2:45pm'),
+                (datetime.time(15, 0, 0), '3:00pm'),
+                (datetime.time(15, 15, 0), '3:15pm'),
+                (datetime.time(15, 30, 0), '3:30pm'),
+                (datetime.time(15, 45, 0), '3:45pm'),
+                (datetime.time(16, 0, 0), '4:00pm'),
+                (datetime.time(16, 15, 0), '4:15pm'),
+                (datetime.time(16, 30, 0), '4:30pm'),
+                (datetime.time(16, 45, 0), '4:45pm'),
+                )
 
 
 class Services(models.Model):
@@ -51,11 +76,19 @@ class Booking(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     date_of_booking = models.DateField()
     service_name = models.ForeignKey(Services, on_delete=models.CASCADE)
-    start_time = models.TimeField(choices=BOOKING_TIME, default=1)
-    end_time = models.TimeField()
+    start_time = models.TimeField(choices=BOOKING_TIME)
+    end_time = models.TimeField(editable=False, blank=True, null=True)
 
     class Meta:
         ordering = ['date_of_booking', 'start_time']
 
     def __str__(self):
         return f"{self.service_name} for {self.username} on {self.date_of_booking} at {self.start_time}"
+
+    def calculateEndTime(self):
+        if self.start_time and self.service_name:
+            start_datetime = datetime.datetime.combine(self.date_of_booking, self.start_time)
+            session_length = self.service_name.session_length
+            end_datetime = start_datetime + session_length
+            self.end_time = end_datetime.time()
+            self.save()
