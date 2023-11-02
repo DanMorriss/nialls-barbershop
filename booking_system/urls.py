a1954a491231a275af3a3f9f0153bffa31e47029
Dan Morriss <danielmorriss1@gmail.com>
2023-11-02 21:39:26 +0000

    Rename index to account-home, removed whitespace below footer and added buttons to upcoming bookings

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.BookingsList.as_view(), name='home')
+    path('', views.BookingsList.as_view(), name='account_home')
 ]
--- booking_system/views.py
+++ booking_system/views.py
@@ -6,6 +6,6 @@ from datetime import date
 
 class BookingsList(generic.ListView):
     model = Booking
-    template_name = 'index.html'
+    template_name = 'account-home.html'
     paginate_by = 25
     queryset = Booking.objects.filter(date_of_booking__gte=date.today()).order_by('date_of_booking', 'start_time')
