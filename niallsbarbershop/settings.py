465c95904ac02426e6f00e47980cb5e1412bcf25
Dan Morriss <danielmorriss1@gmail.com>
2023-11-05 09:07:41 +0000

    Add ability to signin with email and add bookings to navbar

--- niallsbarbershop/settings.py
+++ niallsbarbershop/settings.py
@@ -57,6 +57,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
