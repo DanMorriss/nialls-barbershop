57dcca1d459950e6e9613ccf6edb84a88e19fc84
Dan Morriss <danielmorriss1@gmail.com>
2023-11-09 15:32:33 +0000

    Add about section to index.html

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('booking/', views.BookingsList.as_view(),
          name='booking_home'),
     path('create-booking/', views.CreateBookingView.as_view(),
-         name='create_booking')
+         name='create_booking'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from booking_system.models import Services
 
 
 class HomeView(TemplateView):
     template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = Services.objects.all()
+        return context
